89d7361fcacd2f0b82922c7f8c7c42298ff945c7
max <punkimen666@gmail.com>
2021-09-07 20:58:49 +0300

    UKBank design

---
+++ #src/js/vendor.js
@@ -0,0 +1,5 @@
+@@include('./_jquery-ui.min.js');
+@@include('./_jquery.magnific-popup.min.js');
+@@include('./_jquery.mask.min.js');
+@@include('./_jquery.ui.touch-punch.min.js');
+@@include('./_slick.min.js');
